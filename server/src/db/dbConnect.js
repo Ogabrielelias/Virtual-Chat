1101096611ffa2f49c370d457e604def9bea9ecc
Gabriel Elias <64456754+Ogabrielelias@users.noreply.github.com>
2023-06-22 18:30:46 -0300

    Update dbConnect.js

--- server/src/db/dbConnect.js
+++ server/src/db/dbConnect.js
@@ -14,14 +14,7 @@ const db = {
               senha: 'be8cc789ab6b4b328cb0b9231d35e5421a79d5503f67db3659eb95df82cff0c8e11268ecb43fd55f771cf048045f3131b96467e2d7eca1dc8bbdb471e9527456',
               salSenha: '94e6007239154c922a85e69ecadf926f',
               id: 0
-            },{
-                nome: 'Gabriel Elias Souza de Oliveira',
-                telefone: '51996870825',
-                email: 'gabrielelias8@hotmail.com',
-                senha: 'be8cc789ab6b4b328cb0b9231d35e5421a79d5503f67db3659eb95df82cff0c8e11268ecb43fd55f771cf048045f3131b96467e2d7eca1dc8bbdb471e9527456',
-                salSenha: '94e6007239154c922a85e69ecadf926f',
-                id: 1
-              },
+            },
             
         ],
         chats:[
